6d3f33b92570e74a803fd75661df33a0737e3740
Ilias Tsangaris <iliastsangaris@gmail.com>
2016-01-27 15:43:39 -0800

    add form

--- app/index.jsx
+++ app/index.jsx
@@ -3,14 +3,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Hello extends React.Component {
+class CampSiteSearch extends React.Component {
   render() {
     return (
-      <div>
-        Hello Saturday!
-      </div>
+      <form id="camp-site-search" onSubmit={this.submit}>
+        <input type="text" placeholder=""></input>
+        <input type="submit"></input>
+      </form>
     )
+  }
+
+  submit(e) {
+    e.preventDefault();
+    console.log("submit")
   }
 }
 
-ReactDOM.render(<Hello></Hello>, document.getElementById("root"));
+ReactDOM.render(<CampSiteSearch/>, document.getElementById("root"));
