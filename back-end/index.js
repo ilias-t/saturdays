7d0604410419829b6fc095158682b9e07860ea50
Ilias Tsangaris <iliastsangaris@gmail.com>
2016-02-04 22:31:40 -0800

    hit api from params

--- back-end/index.js
+++ back-end/index.js
@@ -1,8 +1,27 @@
-var app = require('express')();
+var server = require('express')();
+var request = require('request');
 var apiKeys = require('./api-keys');
 
-console.log(apiKeys);
-
-app.get('/campsites/:id', function(req, res) {
-  res.send("campsite" + id);
+server.get('/', function(req, res) {
+  res.send("HELLO!")
 })
+
+// Create a new campsite to track
+server.get('/campsites/:name', function(req, res) {
+  var baseURL = "http://api.amp.active.com/camping/campgrounds/?pstate=CA&pname=" + req.params.name + "&api_key=" + apiKeys.campgroundSearch;
+  request(baseURL, function(err, res, body) {
+    if (!err && res.statusCode === 200) {
+      console.log("success");
+    } else {
+      console.log("error");
+    }
+  });
+});
+
+server.listen(8000, function() {
+  console.log("server listening on 8000");
+});
+
+
+//xml2json
+//https://github.com/Leonidas-from-XIV/node-xml2js
