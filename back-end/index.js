b54106ba3342fa957965fc21d882052f120cf28b
Ilias Tsangaris <iliastsangaris@gmail.com>
2016-02-06 22:47:58 -0800

    front end can hit backend through form

--- back-end/index.js
+++ back-end/index.js
@@ -1,13 +1,22 @@
-var server = require('express')();
+var app = require('express')();
 var request = require('request');
 var apiKeys = require('./api-keys');
 
-server.get('/', function(req, res) {
-  res.send("HELLO!")
-})
+//App settings
+app.use(function(req, res, next) {
+  //CORS
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080"); //whitelist port 8080
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+//Root
+app.get('/', function(req, res) {
+  res.send("HELLO!");
+});
 
 // Create a new campsite to track
-server.get('/campsites/:name', function(req, res) {
+app.get('/campsites/:name', function(req, res) {
   var baseURL = "http://api.amp.active.com/camping/campgrounds/?pstate=CA&pname=" + req.params.name + "&api_key=" + apiKeys.campgroundSearch;
   request(baseURL, function(err, res, body) {
     if (!err && res.statusCode === 200) {
@@ -18,7 +27,8 @@ server.get('/campsites/:name', function(req, res) {
   });
 });
 
-server.listen(8000, function() {
+//Start server
+app.listen(8000, function() {
   console.log("server listening on 8000");
 });
 
--- front-end/app/index.jsx
+++ front-end/app/index.jsx
@@ -1,7 +1,7 @@
-"use strict";
+"use strict"
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 class CampSiteSearch extends React.Component {
   render() {
@@ -13,10 +13,17 @@ class CampSiteSearch extends React.Component {
     )
   }
 
-  submit(e) {
-    e.preventDefault();
-    console.log("submit")
+  submit(e) { //form submit event
+    e.preventDefault() //prevent form submission
+    var request = new XMLHttpRequest()
+    request.onreadystatechange = () => { //event listener to track request's state
+      if (request.readyState === 4) { //request state is set to complete
+        console.log(request.responseText)
+      }
+    }
+    request.open("GET", "http://localhost:8000") //initialize request
+    request.send(null) //send request
   }
 }
 
-ReactDOM.render(<CampSiteSearch/>, document.getElementById("root"));
+ReactDOM.render(<CampSiteSearch/>, document.getElementById("root"))
