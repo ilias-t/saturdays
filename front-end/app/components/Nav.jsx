cadcd81a294775f6f00f7bd9fbec838dd3011934
Ilias Tsangaris <iliastsangaris@gmail.com>
2016-02-07 01:04:40 -0800

    wording

--- front-end/app/components/Nav.jsx
+++ front-end/app/components/Nav.jsx
@@ -5,7 +5,7 @@ export default class Nav extends React.Component {
   render () {
     return (
       <ul>
-        <li><Link to="/">Track Campsites</Link></li>
+        <li><Link to="/">Track a New Campsite</Link></li>
         <li><Link to="/calendar">Book an Upcoming Trip</Link></li>
       </ul>
     )
