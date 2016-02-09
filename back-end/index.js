775fcf85d398445ac49fa3e43ca47e9fac04b4ca
Ilias Tsangaris <iliastsangaris@gmail.com>
2016-02-08 16:42:16 -0800

    add body parser to post campsite

--- back-end/index.js
+++ back-end/index.js
@@ -1,5 +1,6 @@
 var app = require('express')();
 var request = require('request');
+var bodyParser = require('body-parser');
 var apiKeys = require('./api-keys');
 
 //App settings
@@ -10,10 +11,18 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 
 //Root
 app.get('/', function(req, res) {
   res.send("HELLO!");
+});
+
+app.post('/campsites', function(req, res) {
+  console.log("posting");
 });
 
 // Create a new campsite to track
