7adb4f3f1074796e40abcdb93be92bc4c18f35d4
Ilias Tsangaris <iliastsangaris@gmail.com>
2016-02-28 12:47:37 -0800

    module route file

--- back-end/config/routes.js
+++ back-end/config/routes.js
@@ -1,1 +1,18 @@
-routes.js
+module.exports = function routes(app) {
+  //Root
+  app.get('/', function(req, res) {
+    res.send("HELLO!");
+  });
+
+  // Create a new campsite to track
+  app.get('/campsites/:name', function(req, res) {
+    var baseURL = "http://api.amp.active.com/camping/campgrounds/?pstate=CA&pname=" + req.params.name + "&api_key=" + apiKeys.campgroundSearch;
+    request(baseURL, function(err, res, body) {
+      if (!err && res.statusCode === 200) {
+        console.log("success");
+      } else {
+        console.log("error");
+      }
+    });
+  });
+}
--- back-end/index.js
+++ back-end/index.js
@@ -11,22 +11,8 @@ app.use(function(req, res, next) {
   next();
 });
 
-//Root
-app.get('/', function(req, res) {
-  res.send("HELLO!");
-});
-
-// Create a new campsite to track
-app.get('/campsites/:name', function(req, res) {
-  var baseURL = "http://api.amp.active.com/camping/campgrounds/?pstate=CA&pname=" + req.params.name + "&api_key=" + apiKeys.campgroundSearch;
-  request(baseURL, function(err, res, body) {
-    if (!err && res.statusCode === 200) {
-      console.log("success");
-    } else {
-      console.log("error");
-    }
-  });
-});
+// routes
+require('./config/routes')(app);
 
 //Start server
 app.listen(port, function() {
